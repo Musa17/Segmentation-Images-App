4140c5558b9a7f2149fca2af35c0295d2094ae0b
Musa17 <amusul00@fesb.hr>
2021-10-15 21:35:23 +0200

    Adding statistics

--- Image Segmentation/Image_Segmentation.py
+++ Image Segmentation/Image_Segmentation.py
@@ -12,16 +12,26 @@ image_gray_segmented = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 retci = image_gray_segmented.shape[0]
 stupci = image_gray_segmented.shape[1]
 
+klasa1 = 0
+klasa2 = 0
+klasa3 = 0
+
 # Pixel transformations - Segmenting image
 for i in range(0, retci):
 	for j in range(0, stupci):
 		if (image_gray_segmented[i, j] >= 0 and image_gray_segmented[i, j] <= 85):
 			image_gray_segmented[i, j] = 0
+			klasa1 += 1
 		elif (image_gray_segmented[i, j] >= 86 and image_gray_segmented[i, j] <= 171):
 			image_gray_segmented[i, j] = 128
+			klasa2 += 1
 		else:
 			image_gray_segmented[i, j] = 255
+			klasa3 += 1
 
+print('\nBroj piksela u klasi 1 = ' + str(klasa1))
+print('\nBroj piksela u klasi 2 = ' + str(klasa2))
+print('\nBroj piksela u klasi 3 = ' + str(klasa3))
 
 # Show original grayscale image
 cv2.namedWindow('Original gray image')
