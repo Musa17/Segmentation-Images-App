e5077b079c599b7db371c20d07bfbd9a74971ab1
Musa17 <amusul00@fesb.hr>
2021-10-15 21:27:20 +0200

    Adding core functionality - Segmenting grayscale image

--- Image Segmentation/Image_Segmentation.py
+++ Image Segmentation/Image_Segmentation.py
@@ -9,6 +9,18 @@ image = cv2.imread('Pikachu.jpg')
 image_gray_original = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image_gray_segmented = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+retci = image_gray_segmented.shape[0]
+stupci = image_gray_segmented.shape[1]
+
+# Pixel transformations - Segmenting image
+for i in range(0, retci):
+	for j in range(0, stupci):
+		if (image_gray_segmented[i, j] >= 0 and image_gray_segmented[i, j] <= 85):
+			image_gray_segmented[i, j] = 0
+		elif (image_gray_segmented[i, j] >= 86 and image_gray_segmented[i, j] <= 171):
+			image_gray_segmented[i, j] = 128
+		else:
+			image_gray_segmented[i, j] = 255
 
 
 # Show original grayscale image
